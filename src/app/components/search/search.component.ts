524a6e0e4901e59b344766b27410d974fa5be442
joel <joelolguin0@gmail.com>
2020-07-23 16:33:28 -0500

    se crea el arreglo artistas

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../service/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SearchComponent {
+artistas: any [] = [];
+  constructor(private spotify: SpotifyService) {}
+  buscar(termino: string) {
+    console.log(termino);
+    this.spotify.getArtista(termino).subscribe( (data: any) => {
+      console.log(data);
+      this.artistas = data;
+    });
   }
-
 }
