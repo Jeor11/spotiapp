21996f3c55dffbe63abf4a16c88c7a5a0e1492f8
joel <joelolguin0@gmail.com>
2020-07-23 16:42:20 -0500

    cambios en el spotify service

--- src/app/service/spotify.service.ts
+++ src/app/service/spotify.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -21,26 +20,13 @@ export class SpotifyService {
   }
 
   getNewRelases() {
-    /*const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQAe9pcabsXCzUUpA7IV-bVGKuSLtxzrqZ7KKsLQ1W51YBQyjO7LewKz8LXcKvYAeNFT7pr3KAijAY4bRiM',
-    });
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases?limit=20', {
-        headers,
-      })
-      .pipe(
-        map((data: any) => {
-          return data.albums.items;
-        })
-      );*/
-
     return this.getQuery('browse/new-releases?limit=20').pipe(
       map((data: any) => {
         return data.albums.items;
       })
     );
   }
+
   getArtista(termino: string) {
     return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
       map((data: any) => {
