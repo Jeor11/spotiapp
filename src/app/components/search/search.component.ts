da1dea17f5595f1c2995ab742fcecd5f473f776d
joel <joelolguin0@gmail.com>
2020-07-26 22:43:55 -0500

    En el componente search se agrego  loading boolean

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -8,12 +8,15 @@ import { SpotifyService } from '../../service/spotify.service';
 })
 export class SearchComponent {
 artistas: any [] = [];
+loading: boolean;
   constructor(private spotify: SpotifyService) {}
   buscar(termino: string) {
     console.log(termino);
+    this.loading = true;
     this.spotify.getArtista(termino).subscribe( (data: any) => {
       console.log(data);
       this.artistas = data;
+      this.loading = false;
     });
   }
 }
